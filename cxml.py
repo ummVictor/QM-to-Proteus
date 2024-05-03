19e4ab2b19f6062ea34a65247cc77b486e2fe5aa
ummVictor <victorum6943208@gmail.com>
2024-05-03 14:42:53 -0700

    finished

--- cxml.py
+++ cxml.py
@@ -1,7 +1,6 @@
 import xml.etree.ElementTree as ET
 import unittest
 
-
 #Next issue: Store how the states connects to each other 
 #   (list of string names for list of states and events) 
 #   (connections are displayed in map format, tuple arrays of entries)
@@ -110,59 +109,60 @@ def traverse(element):
 
 traverse(root)
 
-#print("Number of States: ", len(stateParseList))
-#print("List of States: ", stateParseList)
-#print("List of Transition Targets: ", tranParseList)
-#print("List of Triggers/Events: ", trigParseList)
-#print("StateObject list: ")
-#for i in stateObjList: #Loop through state list
-#    print(" " , i) #Print state
-#    for e in i.transitions: #Loop through current state's list of transitions
-#        print("\t", e)
-#print("end")
+print("Number of States: ", len(stateParseList))
+print("List of States: ", stateParseList)
+print("List of Transition Targets: ", tranParseList)
+print("List of Triggers/Events: ", trigParseList)
+print("StateObject list: ")
+for i in stateObjList: #Loop through state list
+    print(" " , i) #Print state
+    for e in i.transitions: #Loop through current state's list of transitions
+        print("\t", e)
+print("end")
 
 # Open a text file in write mode
-#with open('output.txt', 'w') as f:
+with open('output.proteus', 'w') as f:
     # Write Proteus header
-#    f.write("//Proteus\n\n")
+    f.write("//Proteus\n\n")
+
     # Write beginning of the List of Triggers/Events
-#    f.write("//beginning of the List of Triggers/Event\n")
-#    for trigger_name in trigParseList:
-#        # Ensure consistent format for triggers
-#        if trigger_name.endswith("()"):
-#            f.write(f"event {trigger_name}\n")
-#        else:
-#            f.write(f"event {trigger_name};\n")
-#    f.write("//end of the List of Triggers/Event\n\n")
+    f.write("//beginning of the List of Triggers/Event\n")
+    for trigger_name in trigParseList:
+        # Ensure consistent format for triggers
+        if trigger_name.endswith("()"):
+            f.write(f"event {trigger_name}\n")
+        else:
+            f.write(f"event {trigger_name};\n")
+    f.write("//end of the List of Triggers/Event\n\n")
 
     # Write actor block with actor name 'learn'
-#    f.write("actor learn{\n")
+    f.write("actor learn{\n")
 
     # Write statemachine block
-#   f.write("    statemachine {\n")
+    f.write("    statemachine {\n")
     
     # Write the initial state as 'initial Locked;'
-#    f.write("        initial Locked;\n")
+    f.write("        initial Locked;\n")
     
     # Iterate through state object list to write state blocks
-#    for state in stateObjList:
+    for state in stateObjList:
         # Write state name
-#        f.write(f"        state {state.stateName} {{\n")
+        f.write(f"        state {state.stateName} {{\n")
         
         # Iterate through each transition in the state
-#        for transition in state.transitions:
-#            trigger_name = transition.trigger.triggerName
+        for transition in state.transitions:
+            trigger_name = transition.trigger.triggerName
             # Find the destination state
-#            destination_state = findDestination(transition)
+            destination_state = findDestination(transition)
             
             # Write the transition block
-#            f.write(f"            on {trigger_name} {{\n")
-#            f.write(f"                go {destination_state};\n")
-#            f.write(f"            }}\n")
+            f.write(f"            on {trigger_name} {{\n")
+            f.write(f"                go {destination_state};\n")
+            f.write(f"            }}\n")
         
         # Close the state block
-#        f.write("        }\n")
+        f.write("        }\n")
     
     # Close statemachine and actor blocks
-#    f.write("    }\n")
-#    f.write("}\n")S
+    f.write("    }\n")
+    f.write("}\n")
