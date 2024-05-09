6226143bf8db6375173f30c35e1ab9fe6a070258
Victor Um <127917615+ummVictor@users.noreply.github.com>
2024-05-09 10:25:51 -0700

    Update cxml.py

--- cxml.py
+++ cxml.py
@@ -6,7 +6,7 @@ import unittest
 #   (list of string names for list of states and events) 
 #   (connections are displayed in map format, tuple arrays of entries)
 
-tree = ET.parse('learn.qm')
+tree = ET.parse('statemachine.qm')
 root = tree.getroot()
 
 stateParseList = []
@@ -110,59 +110,3 @@ def traverse(element):
 
 traverse(root)
 
-#print("Number of States: ", len(stateParseList))
-#print("List of States: ", stateParseList)
-#print("List of Transition Targets: ", tranParseList)
-#print("List of Triggers/Events: ", trigParseList)
-#print("StateObject list: ")
-#for i in stateObjList: #Loop through state list
-#    print(" " , i) #Print state
-#    for e in i.transitions: #Loop through current state's list of transitions
-#        print("\t", e)
-#print("end")
-
-# Open a text file in write mode
-#with open('output.txt', 'w') as f:
-    # Write Proteus header
-#    f.write("//Proteus\n\n")
-    # Write beginning of the List of Triggers/Events
-#    f.write("//beginning of the List of Triggers/Event\n")
-#    for trigger_name in trigParseList:
-#        # Ensure consistent format for triggers
-#        if trigger_name.endswith("()"):
-#            f.write(f"event {trigger_name}\n")
-#        else:
-#            f.write(f"event {trigger_name};\n")
-#    f.write("//end of the List of Triggers/Event\n\n")
-
-    # Write actor block with actor name 'learn'
-#    f.write("actor learn{\n")
-
-    # Write statemachine block
-#   f.write("    statemachine {\n")
-    
-    # Write the initial state as 'initial Locked;'
-#    f.write("        initial Locked;\n")
-    
-    # Iterate through state object list to write state blocks
-#    for state in stateObjList:
-        # Write state name
-#        f.write(f"        state {state.stateName} {{\n")
-        
-        # Iterate through each transition in the state
-#        for transition in state.transitions:
-#            trigger_name = transition.trigger.triggerName
-            # Find the destination state
-#            destination_state = findDestination(transition)
-            
-            # Write the transition block
-#            f.write(f"            on {trigger_name} {{\n")
-#            f.write(f"                go {destination_state};\n")
-#            f.write(f"            }}\n")
-        
-        # Close the state block
-#        f.write("        }\n")
-    
-    # Close statemachine and actor blocks
-#    f.write("    }\n")
-#    f.write("}\n")S
